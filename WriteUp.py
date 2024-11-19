a905af8646d7e4fee10217c70b95c01948ff96cf
Molly Shove <Molly.Shove@microsoft.com>
2024-11-18 18:17:09 -0800

    Worked on generating a modular json to thow into the Open AI API.
    It prints! but doesn't yet write out to file

--- WriteUp.py
+++ WriteUp.py
@@ -3,21 +3,36 @@ from openai import OpenAI
 import os
 import json
 
+#This is the file which will be more text than code. 
+
+#load in the API key
+client = OpenAI(api_key=os.getenv("SUE_API_KEY"))
+
+# Load the text from the parsed events file
+with open("parsed_events.txt", "r") as file:
+    content = file.read()
+
+user = "You are an enthusiastic hockey fan giving an update on the toronto maple leaves! You know all the nicknames of the players, the slang and are excited when they win and angry when they loose."
+system = "write a 500 words of highlights of the tuseday game. Include the included information about goals and pentalties. don't forget to have fun!"
+model = "gpt-4o-mini"
 
 
+# use the above to create a structure message.json file
+with open("message.json", "w") as file:
+    file.write("[")
+    json.dump({"role": "user", "content": user}, file)
+    file.write(",")
+    file.write("\n")
+    json.dump({"role": "system", "content": system + content}, file)
+    file.write("]")
 
 
 client = OpenAI(api_key=os.getenv("SUE_API_KEY"))
 
 completion = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[
-        {"role": "system", "content": "You are an enthusiastic hockey fan giving an update on the toronto maple leaves! You know all the nicknames of the players, the slang and are excited when they win and angry when they loose."},
-        {
-            "role": "user",
-            "content": "write a 300 words of highlights of the tuseday game. Include the score and who scored the goals."
-     }
- ]
+    model=model,
+    messages= json.load(open("message.json"))
 )
 
-# print(completion.choices[0].message) 
+print(completion.choices[0].message)
+#write the content out to file
