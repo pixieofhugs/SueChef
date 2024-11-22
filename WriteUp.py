1d95ee2a388928c5b60db076c5fc9ced2bc7a827
Molly Shove <Molly.Shove@microsoft.com>
2024-11-21 23:14:53 -0800

    Tested with User. Did prompt engeneering. This is probably more tokens than I want to use but it produced a better result

--- WriteUp.py
+++ WriteUp.py
@@ -12,8 +12,30 @@ client = OpenAI(api_key=os.getenv("SUE_API_KEY"))
 with open("parsed_events.txt", "r") as file:
     content = file.read()
 
-user = "You are an enthusiastic hockey fan giving an update on the toronto maple leaves! You know all the nicknames of the players, the slang and are excited when they win and angry when they loose."
-system = "write a 500 words of highlights of the tuseday game. Include the included information about goals and pentalties. don't forget to have fun!"
+hocky_errata = """ Here are some things you know about Hockey: 
+- Each period is 20 minutes long
+- Overtime is 5 minutes long
+-     
+    """
+
+leafs_errata = """ Here are some things you know about the Toronto Maple Leafs:
+ - John Tavares is no longer the captain, Auston Matthews is the captain
+ - The team coach is Craig Berube
+ - B. McMann is nicknamed Bobby "The Man" McMann
+ - Very occationally, the team is called the "Buds"
+ - Mitch Marner is occationally called "Shambles"
+ - Auston Matthews is occationally called "Papi"
+ - Morgan Rielly is occationally called "Riles" or "Captain Morgan"
+ - William Nylander is occationally called "Willy" or "Nylanderthall"
+ - Storlarz is occationally called "Stellaris" or "Stolie the Goalie"
+ - Mathew Knies is occationally called "The Mutant"
+ - John Tavares is occationally called "JT" or "Johnny Toronto". He is often called "The Amulet"
+"""
+
+user = """You are an enthusiastic hockey fan giving an update on the toronto maple leaves! You know all the nicknames of the players, 
+            the slang and are excited when they win and angry when they loose."""
+system = """You are announcer Joe Bowen giving a lively 500 word retelling of a Hockey game. Use highlights of the provided game using the event information. 
+            Include the included information about goals and pentalties. don't forget to have fun!"""
 model = "gpt-4o-mini"
 
 
@@ -24,6 +46,12 @@ with open("message.json", "w") as file:
     file.write(",")
     file.write("\n")
     json.dump({"role": "system", "content": system + content}, file)
+    file.write(",")
+    file.write("\n")
+    json.dump({"role": "system", "content": hocky_errata}, file)
+    file.write(",")
+    file.write("\n")
+    json.dump({"role": "system", "content": leafs_errata}, file)
     file.write("]")
 
 
@@ -35,4 +63,12 @@ completion = client.chat.completions.create(
 )
 
 print(completion.choices[0].message)
-#write the content out to file
+
+# convert the comlpetion message to a string
+completion_message = str(completion.choices[0].message)
+
+# write the completion message to a file
+with open("writeup.txt", "w") as file:
+    file.write(completion_message)
+
+
